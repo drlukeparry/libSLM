532d7d273d649653f2bb60548678a841f73d4581
Luke Parry <dev@lukeparry.uk>
2020-05-26 18:40:24 +0100

    Update setup.py and moved python module to build a python package
    Signed-off-by: Luke Parry <dev@lukeparry.uk>

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ import sys
 import platform
 import subprocess
 
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
 
+with open('README.rst') as f:
+    readme = f.read()
+
+
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
+    def __init__(self, name, modName, sourcedir=''):
+        
+        Extension.__init__(self, name, sources=[], language='c++')
+        
+        self.modName = modName
         self.sourcedir = os.path.abspath(sourcedir)
 
 
@@ -32,11 +39,13 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
+        print('ext_full_path', self.get_ext_fullpath(ext.name))
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         # required for auto-detection of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
+        print('extdir', extdir)
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
@@ -52,23 +61,49 @@ class CMakeBuild(build_ext):
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
+        cmake_args += ['-DCMAKE_INSTALL_PREFIX=' + '.']
+        
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', ext.modName] + build_args, cwd=self.build_temp)
+        
+       # subprocess.check_call(['cmake', '--build', '.', '--target', 'install'], cwd=self.build_temp)
+        
 
 setup(
-    name='cmake_example',
-    version='0.0.1',
-    author='Dean Moldovan',
-    author_email='dean0x7d@gmail.com',
-    description='A test project using pybind11 and CMake',
-    long_description='',
-    ext_modules=[CMakeExtension('cmake_example')],
+    name='libSLM',
+    version='0.1.0',
+    author='Luke Parry',
+    author_email='dev@lukeparry.uk',
+    url='https://github.com/drlukeparry/libslm',
+    long_description=readme,
+    long_description_content_type = 'text/x-rst',
+    description='libSLM is a python library for reading and writing to common machine build files used by commerical Additive Manufacturing systems',
+    ext_modules=[CMakeExtension('libSLM.slm', 'slm'), 
+                 CMakeExtension('libSLM.translators.mtt', 'mtt')],
     cmdclass=dict(build_ext=CMakeBuild),
+    packages = ['libSLM', 'libSLM.translators'],
+    package_dir={'': 'python'},
+    keywords=['3D Printing', 'AM', 'Additive Manufacturing', 'Geometry', 'SLM', 'Selective Laser Melting'],
+    # tell setuptools that all packages will be under the 'src' directory
+    # and nowhere else
     zip_safe=False,
+        classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering'],
+    license="",
+    project_urls = {
+    'Documentation': 'https://pyslm.readthedocs.io/en/latest/',
+    'Source': 'https://github.com/drylukeparry/libslm',
+    'Tracker': 'https://github.com/drlukeparry/libslm/issues'
+    }
 )
  
